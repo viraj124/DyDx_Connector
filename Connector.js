2c248ef368f65ec1735cf023a8f0c88ca65d53d0
Viraz Malhotra <virajm72@gmail.com>
2020-03-14 18:13:01 +0530

    Deposit and Borrow Added

--- Connector.js
+++ Connector.js
@@ -32,15 +32,16 @@ class App extends Component {
   async loadBlockchainData() {
     const web3 = window.web3
 
-    var dydx_operation =  {"constant":false,"inputs":[{"components":[{"name":"owner","type":"address"},{"name":"number","type":"uint256"}],"name":"accounts","type":"tuple[]"},{"components":[{"name":"actionType","type":"uint8"},{"name":"accountId","type":"uint256"},{"components":[{"name":"sign","type":"bool"},{"name":"denomination","type":"uint8"},{"name":"ref","type":"uint8"},{"name":"value","type":"uint256"}],"name":"amount","type":"tuple"},{"name":"primaryMarketId","type":"uint256"},{"name":"secondaryMarketId","type":"uint256"},{"name":"otherAddress","type":"address"},{"name":"otherAccountId","type":"uint256"},{"name":"data","type":"bytes"}],"name":"actions","type":"tuple[]"}],"name":"operate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}
+    var dydx_operation =  {"constant":false,"inputs":[{"components":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"number","type":"uint256"}],"internalType":"struct DydxInterface.Info[]","name":"accounts","type":"tuple[]"},{"components":[{"internalType":"enum DydxInterface.ActionType","name":"actionType","type":"uint8"},{"internalType":"uint256","name":"accountId","type":"uint256"},{"components":[{"internalType":"bool","name":"sign","type":"bool"},{"internalType":"enum DydxInterface.AssetDenomination","name":"denomination","type":"uint8"},{"internalType":"enum DydxInterface.AssetReference","name":"ref","type":"uint8"},{"internalType":"uint256","name":"value","type":"uint256"}],"internalType":"struct DydxInterface.AssetAmount","name":"amount","type":"tuple"},{"internalType":"uint256","name":"primaryMarketId","type":"uint256"},{"internalType":"uint256","name":"secondaryMarketId","type":"uint256"},{"internalType":"address","name":"otherAddress","type":"address"},{"internalType":"uint256","name":"otherAccountId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"internalType":"struct DydxInterface.ActionArgs[]","name":"actions","type":"tuple[]"},{"internalType":"uint256","name":"maxAmt","type":"uint256"}],"name":"borrow","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}
 
     var accounts = [["0x2a5c6E0Eb76915466C0CE771DCFb6f258a572336", "223"]];
-
-    var actions = [["1", "0", [false, "0", "1", "1000"], "1", "99", "0x2a5c6E0Eb76915466C0CE771DCFb6f258a572336", "99", web3.utils.asciiToHex("0")]];
+    //first deposit and then withdraw
+    var actions = [["0", "0", [true, "0", "0", "10000000"], "0", "98", "0x2a5c6E0Eb76915466C0CE771DCFb6f258a572336", "98", web3.utils.asciiToHex("0")], ["1", "0", [false, "0", "0", "1000"], "1", "99", "0x2a5c6E0Eb76915466C0CE771DCFb6f258a572336", "99", web3.utils.asciiToHex("0")]];
 
     var dydx_operation_args = [
       accounts,
-      actions
+      actions,
+      "100000000000"
     ]
 
 
