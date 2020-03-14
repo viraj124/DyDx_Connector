413b3688325cff08664473812c9ef2a97951db31
Viraz Malhotra <virajm72@gmail.com>
2020-03-14 18:13:40 +0530

    Deposit and Borrow Added

--- Connector.sol
+++ Connector.sol
@@ -55,16 +55,97 @@ contract DydxInterface {
         
 }
 
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
 contract Helper {
     function getOperation() public pure returns (address operation) {
         operation = 0x4EC3570cADaAEE08Ae384779B0f3A45EF85289DE;
+    }
+    
+    function getWETH() public pure returns (address weth) {
+        weth = 0xd0A1E359811322d97991E03f863a0C30C2cF029C;
     }
 }
 
 
 contract Connector is Helper, DydxInterface{
-    function operate(Info[] memory accounts, ActionArgs[] memory actions) public {
-        DydxInterface(getOperation()).operate(accounts, actions);
+    
+    function borrow(Info[] memory accounts, ActionArgs[] memory actions, uint maxAmt) public {
+        IERC20(getWETH()).approve(getOperation(), maxAmt);
+        operate(accounts, actions);
    }
+   
+    function operate(Info[] memory accounts, ActionArgs[] memory actions) public {
+            DydxInterface(getOperation()).operate(accounts, actions);
+    }
 }
 
